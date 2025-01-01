1a55b1b3d07031a170111fcaca91250625877186
Tanisha Jauhari <92215165+Tanisboxed@users.noreply.github.com>
2025-01-02 01:30:58 +0530

    Update product-recommend.tsx

--- components/product/product-recommend.tsx
+++ components/product/product-recommend.tsx
@@ -23,9 +23,9 @@ const recommendedProducts: any[] = [
   },
   {
     _id: "3",
-    title: "Sleek Womens Jeans",
+    title: "Sleek black shiny boots shoes",
     price: 579,
-    images: ["https://plus.unsplash.com/premium_photo-1689371952452-c88c72464115?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"],
+    images: ["https://plus.unsplash.com/premium_photo-1671379102281-7225f3d3d97d?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"],
     rating: 4.6,
     reviewCount: 45,
   },
